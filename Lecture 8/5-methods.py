0c9a7ac7d73dc7b6ded02c91c892112abf7c467a
Rishi <rishilord2@gmail.com>
2024-11-07 22:34:07 +0530

    add lecture 8

--- Lecture 8/5-methods.py
+++ Lecture 8/5-methods.py
@@ -1,19 +1,31 @@
 # methods in oop
 # methods are functions that belongs to oop
-# user can create their own method
+# user can also create their own method
 
+# class Student:
+#     def __init__(self, name, marks):
+#        self.name = name
+#        self.marks = marks 
+#        print("adding new student in databse....") 
 
-class Student:
-    def __init__(self, name, marks):
-       self.name = name
-       self.marks = marks 
-       print("adding new student in databse....") 
+#     def welcome(self): # method
+#         print("welcome dear", self.name)
+#     def get_marks(self):
+#         return self.marks
 
-    def welcome(self): # method
-        print("welcome dear", self.name)
-    def get_marks(self):
-        return self.marks
+# s1 = Student("Karan", 97)
+# s1.welcome() # calling method
+# print(s1.get_marks())
 
-s1 = Student("Karan", 97)
-s1.welcome() # calling method
-print(s1.get_marks())
+# static method
+# methods that dont use the self parameter
+# syntax
+class Design:
+    @staticmethod # decorator
+    def college():
+        print("ABC College")
+    
+p1 = Design()
+p1.college()
+
+# decorators allows to wrap another function in order to extend the behaviour of the wrapped function, without parmanently modifying it
