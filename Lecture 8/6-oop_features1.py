f2a382a0933c15143ba496b644b1bf723b15ee5b
Rishi <rishilord2@gmail.com>
2024-11-08 21:57:31 +0530

    add new files

--- Lecture 8/6-oop_features1.py
+++ Lecture 8/6-oop_features1.py
@@ -1,6 +1,6 @@
 # Features in Oops
-# i) abstruction
+# 1. abstruction
 # abstruction means hiding of class and only showing the essentual features to the user
 
-# ii) encapsulation
+# 2. encapsulation
 # wrapping data and function in single unit ( which is object)
--- Lecture 9/6-oop_features2.py
+++ Lecture 9/6-oop_features2.py
@@ -1,31 +1,45 @@
 # Features in Oops
-# iii) inheritence
+# 3. inheritence
 # when one class is derives the properties & methods of another class
 # the main class is call parent/base class & derived class is call child/derived class
 
-class Car:
-    @staticmethod
-    def start():
-        print("Car started...")
+# types in inheritence
+# i) signle level -> 1 base class & 1 dervied class 
+# ii) multi-level -> 1 base class & more than 1 derived class which are dervied from derived class
+# ii) multiple level -> more than 1 base class and they gets inherit by child class
 
-    @staticmethod
-    def stop():
-        print("Car stopped.")
+# example 1 :
+# class Car:
+#     @staticmethod
+#     def start():
+#         print("Car started...")
 
-class ToyotaCar(Car):
-    def __init__(self, name, color):
-        self.name = name
-        self.color = color
+#     @staticmethod
+#     def stop():
+#         print("Car stopped.")
 
-    def start_car(self):
-        Car.start()
+# class ToyotaCar(Car): # single level inheritance
+#     def __init__(self, brand, color):
+#         self.brand = brand
+    
+# class Fortuner(ToyotaCar): # multi-level inheritance
+#     def __init__(self, type):
+#         self.type = type
 
-    def stop_car(self):
-        Car.stop()
+# car1 = Fortuner("Disel")
+# car1.start()
 
-car1 = ToyotaCar("Fortuner", "Black")
-car2 = ToyotaCar("Prius", "Red")
+# example 2 :
+class A:
+    varA = "welcome to class A"
 
-car1.start_car()
-print("Car name is",car1.name,"\nCar color is",car1.color)
-car1.stop_car()
+class B:
+    varB = "welcome to class B"
+
+class C(A, B):
+    varC = "welcome to class C"
+
+c1 = C() # multiple inheritance
+print(c1.varA)
+print(c1.varB)
+print(c1.varC)
