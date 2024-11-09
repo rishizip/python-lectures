7ad5f079298373c85780019072db3deb5594a519
Rishi <rishilord2@gmail.com>
2024-11-09 15:48:25 +0530

    added new codes

--- Lecture 9/1-oop_methods2.py
+++ Lecture 9/1-oop_methods2.py
@@ -14,16 +14,77 @@
 # syntax
 # _private = "variable"
 
-class Person:
-    def __init__(self, name, acc_no, acc_pass):
-        self.name = name
-        self.acc_no = acc_no
-        self.__acc_pass = acc_pass
+# class Person:
+#     def __init__(self, name, acc_no, acc_pass):
+#         self.name = name
+#         self.acc_no = acc_no
+#         self.__acc_pass = acc_pass
        
-    def check(self):
-        return self.__acc_pass
+#     def check(self):
+#         return self.__acc_pass
     
-acc1 = Person("rishizip", 12345678, "abcdefgh")
-print(acc1.name)
-print(acc1.acc_no)
-print(acc1.check())
+# acc1 = Person("rishizip", 12345678, "abcdefgh")
+# print(acc1.name)
+# print(acc1.acc_no)
+# print(acc1.check())
+
+# 3. super() -> super method is used to acess mrthods of parent class
+# class Car:
+#     def __init__(self, type):
+#         self.type = type
+
+#     @staticmethod
+#     def start():
+#         print("Car started...")
+
+#     @staticmethod
+#     def stop():
+#         print("Car stopped.")
+
+# class ToyotaCar(Car):
+#     def __init__(self, name, type):
+#         self.name = name
+#         super().__init__(type)
+
+# car1 = ToyotaCar("prius", "electric")
+# print(car1.type)
+
+# 4. class method -> abound to class & recives the class as an implicit first argument
+# here cls pass as first argument and it indicates base class
+# class Person:
+#     name = "Your Name"
+
+#     @classmethod
+#     def ChangeName(cls, name):
+#         cls.name = name
+
+# p1 = Person()
+# p1.ChangeName("Rahul Kumar")
+# print(p1.name)
+# print(Person.name)
+# also change the name in person class
+
+# summary of methods
+# i) static methods -> used when we dont want to use any properties of class or other methods
+# ii) class methods(cls) -> use when we want use only class attributes and want to change directly on class
+# iii) instance methods(self) -> use when we want to use instance property
+
+# property decorator
+class Student:
+    def __init__(self, chem, phy, math):
+        self.chem = chem
+        self.phy = phy
+        self.math = math
+
+    # def calpercentage(self):
+    #     self.percentage = str((self.chem + self.math + self.phy) / 3) + "%"
+
+    @property
+    def percentage(self):
+        return str((self.chem + self.math + self.phy) / 3) + "%"
+        
+stu1 = Student(98, 99, 92)
+print(stu1.percentage)
+
+stu1.phy = 86
+print(stu1.percentage)
--- Lecture 9/6-oop_features2.py
+++ Lecture 9/6-oop_features2.py
@@ -30,16 +30,54 @@
 # car1.start()
 
 # example 2 :
-class A:
-    varA = "welcome to class A"
+# class A:
+#     varA = "welcome to class A"
 
-class B:
-    varB = "welcome to class B"
+# class B:
+#     varB = "welcome to class B"
 
-class C(A, B):
-    varC = "welcome to class C"
+# class C(A, B):
+#     varC = "welcome to class C"
 
-c1 = C() # multiple inheritance
-print(c1.varA)
-print(c1.varB)
-print(c1.varC)
+# c1 = C() # multiple inheritance
+# print(c1.varA)
+# print(c1.varB)
+# print(c1.varC)
+
+# 4. polymorphism 
+# when the same operator have different meaning according to the context#
+# polymorphism of +
+# print(1+2)
+# print("rishi" + "zip")
+# print([1, 2, 3] + [4 , 5 , 6])
+
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    def show_number(self):
+        print(f"{self.real} + {self.img}j")
+
+    def __add__(self, num2):
+        newReal = self.real + num2.real
+        newImg = self.img + num2.img
+        return Complex(newReal, newImg)
+    
+    def __sub__(self, num2):
+        newReal = self.real - num2.real
+        newImg = self.img - num2.img
+        return Complex(newReal, newImg)
+
+num1 = Complex(1, 3)
+num1.show_number()
+
+num2 = Complex(4, 6)
+num2.show_number()
+
+num3 = num1 + num2
+num3.show_number()
+
+num3 = num1 - num2
+num3.show_number()
+# we can define operator overloading by using dunder function(function who have double underscore on it)
